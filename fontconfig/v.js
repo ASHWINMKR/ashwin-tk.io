863e10cb02569695f666d32524b5479c7301ee12
ASHWINMKR <152799473+ASHWINMKR@users.noreply.github.com>
2024-08-14 10:02:45 +0530

    Add files via upload

---
+++ fontconfig/v.js
@@ -0,0 +1,14 @@
+function googleTranslateElementInit(){
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    console.log('google_translate_element');
+}
+
+setTimeout(function(){
+    cookie.classList.add('active');
+},2000);  
+
+if(userinput == "ashwin"){
+    alert(userinput)
+}
+
+
